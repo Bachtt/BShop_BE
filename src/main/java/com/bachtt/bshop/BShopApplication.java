440e351a3ba4631b01aec61e17b6afef2f0a4823
Bachtt <Bachtt107@gmail.com>
2022-05-23 09:13:20 +0700

    initialized project

---
+++ src/main/java/com/bachtt/bshop/BShopApplication.java
@@ -0,0 +1,13 @@
+package com.bachtt.bshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BShopApplication.class, args);
+    }
+
+}
